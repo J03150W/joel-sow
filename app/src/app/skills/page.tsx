efbe9fa5a9191be3874d5ccafe3aa93f2aa9cb34
J03150W <joelsow247@gmail.com>
2025-04-28 18:16:16 +0200

    added missing "use client"; to skills page

--- app/src/app/skills/page.tsx
+++ app/src/app/skills/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { useLanguage } from "@/context/LanguageContext";
 import { useEffect, useRef, useState } from "react";
 import NoStrictPortal from "../components/noStrictPortal";
